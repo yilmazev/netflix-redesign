bbdc366b1d87fbec47f45ecaf6df16ce79fd32fa
Yılmaz Ev <81462055+yilmazev@users.noreply.github.com>
2023-01-19 16:58:59 +0300

    Bug fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
+        externalDir: true | {
+            enabled: true,
+            silent: true,
+       },
    },
 }
 
